669d8235f5e819eced206bc6899fce2e2ffbdd3d
guilhermepisco19 <g.pisco@campus.fct.unl.pt>
2021-06-12 12:43:32 +0100

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/guilhermepisco/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.guilhermepisco.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.guilhermepisco.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>{
+
+}
