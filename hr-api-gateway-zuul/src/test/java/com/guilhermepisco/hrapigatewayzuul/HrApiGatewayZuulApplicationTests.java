532a18e21973060f17e40670d78a70d46c069875
guilhermepisco19 <g.pisco@campus.fct.unl.pt>
2021-06-17 22:42:40 +0100

    Porject hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/test/java/com/guilhermepisco/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermepisco.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
